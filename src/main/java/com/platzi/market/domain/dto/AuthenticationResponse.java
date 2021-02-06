dbee578d096cbcdef3fd13c7a06466a48b3c1242
macg30 <macg30@users.noreply.github.com>
2021-02-06 15:53:52 +0100

    Add JWT

---
+++ src/main/java/com/platzi/market/domain/dto/AuthenticationResponse.java
@@ -0,0 +1,17 @@
+package com.platzi.market.domain.dto;
+
+public class AuthenticationResponse {
+    public String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+}
